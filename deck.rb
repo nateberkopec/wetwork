575088d2f63e349bf7ed479badc3220461b66764
Nate Berkopec <nate.berkopec@gmail.com>
2015-07-11 16:57:52 -0400

    Three card types. Colors.

--- deck.rb
+++ deck.rb
@@ -1,6 +1,14 @@
 require 'squib'
 
-Squib::Deck.new(cards: 3, layout: 'layout.yml') do
-  text str: 'Hello, World!'
-  save format: :png
+light = '#F3EFE3'
+dark = '#230602'
+
+Squib::Deck.new(cards: 3, width: 1125, height: 825) do
+  background color: dark
+
+  text str: %w( Facility Asset Equipment ),
+       color: light, font: 'Helvetica 64',
+       y: '2.0in', width: '3.75in', align: :center
+
+  save_png
 end
